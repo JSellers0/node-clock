a9bff88eb8e9ab2fd73064dcc503cdf847bd4a6b
Sellerman <Jeffrey.Sellers@maritz.com>
2022-09-24 13:23:05 -0400

    item_select_get for project, task, note

--- src/controllers/adustController.ts
+++ src/controllers/adustController.ts
@@ -2,14 +2,26 @@ import {Request, Response, NextFunction} from 'express'
 import timeclock from '../models/timeclock.model'
 
 export function adjust_get(req: Request, res: Response, next: NextFunction) {
-    //res.render("register.njk", {crsfToken: req.csrfToken(), domain: res.locals.domain})
     res.render("adjust.njk", {domain: res.locals.domain})
 }
 
 export async function item_select_get(req: Request, res: Response, next: NextFunction) {
-    //res.render("register.njk", {crsfToken: req.csrfToken(), domain: res.locals.domain})
-    let items: string[] = await timeclock.get_list(req.params.item_type)
-    res.send(`NOT IMPLEMENTED: item selection GET ${items}`)
+    let item_type: string = req.params.item_type.substring(0, 1).toUpperCase() + req.params.item_type.substring(1, req.params.item_type.length)
+    try {
+        await timeclock.get_list(req.params.item_type)
+            .then((items) => {
+                return {
+                    domain: res.locals.domain,
+                    item_type: item_type,
+                    items: items
+                }
+            }). then((site_components) => {
+                res.render("adjust_itemselect.njk", site_components)
+            })
+    } catch(err) {
+        // ToDo: Real Logging
+        console.log(err)
+    }
 }
 
 export function item_adjust_get(req: Request, res: Response, next: NextFunction) {
